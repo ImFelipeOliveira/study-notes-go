f24864ba0a128af1fb6c222da8898927981e4d51
Felipe Oliveira <flipeaz342@gmail.com>
2025-03-13 17:20:05 -0400

    Study notes initial commit

---
+++ types/values.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Strings
+	fmt.Println("Go" + "Lang")
+
+	// String + Integer + Float
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println("7.0/3.0", 7.0/3.0)
+
+	// Boolean
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
